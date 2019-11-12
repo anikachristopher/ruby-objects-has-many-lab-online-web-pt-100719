bfda1ddc1f1a6d83a2c4a499fd01a9ea20a02aaa
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-12 03:39:42 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -12,9 +12,9 @@ class Post
     @@all
   end
   
-  def artist_name
-    if artist 
-      self.artist.name
+  def author_name
+    if author 
+      self.author.name
     else
       nil 
     end
