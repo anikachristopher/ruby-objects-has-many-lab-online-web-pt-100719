a6b940666acbeb013cdf87324d3459b0cdccd4c5
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-12 03:36:39 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post 
+  attr_accessor :author, :name
+  
+    @@all = []
+    
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    if artist 
+      self.artist.name
+    else
+      nil 
+    end
+  end
+      
+end
