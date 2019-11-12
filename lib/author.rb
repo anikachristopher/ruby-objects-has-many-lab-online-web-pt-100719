3e5df9b6c9b95e191e19608e590e8bf1ba8efa82
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-12 03:35:29 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,27 @@
 class Author
-  attr_accessor :name, :song
+  attr_accessor :name, :post
   
   def initialize(name)
     @name = name
   end
   
-  def songs 
-    Song.all.select do |song|
-    song.artist == self
+  def posts 
+    Posts.all.select do |post|
+    post.author == self
     end
   end
 
-  def add_song(song)
-    song.artist = self
-  end
+#   def add_song(song)
+#     song.artist = self
+#   end
   
-  def add_song_by_name(song_name)
-   performer = Song.new(song_name) 
-   add_song(performer)
-  end
+#   def add_song_by_name(song_name)
+#   performer = Song.new(song_name) 
+#   add_song(performer)
+#   end
   
-  def self.song_count
-    Song.all.count 
-  end
-end
+#   def self.song_count
+#     Song.all.count 
+#   end
+# end
   
